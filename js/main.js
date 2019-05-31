13e7436a0192aeb3b0e07f21d5841e68765c0880
Solvaigh26 <mr.kometo2000@gmail.com>
2019-05-31 19:43:34 +0300

    search weather done

--- js/main.js
+++ js/main.js
@@ -15,20 +15,19 @@ function userLocationWeather () {
             long = position.coords.longitude;
             lat = position.coords.latitude;
             
-            var proxy = "https://cors-anywhere.herokuapp.com/";
             var appId = "a03e764ac19cc224e87a100ab518b340";
-            var api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${appId}`;
+            var api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${appId}`;
         
-        fetch(api).then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            temperatureDegree.innerText = Math.round(data.main.temp - 273,15);
-            temperatureDescription.innerText = data.weather[0].description;
-            weatherIcon.src = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-            locationName.innerText = data.name;
-        }).catch(error => {
-            console.log(error);
+            fetch(api).then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                temperatureDegree.innerText = Math.round(data.main.temp - 273,15)+ " °C";
+                temperatureDescription.innerText = data.weather[0].description;
+                weatherIcon.src = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+                locationName.innerText = data.name;
+            }).catch(error => {
+                console.log(error);
             });
         });
     }
@@ -49,8 +48,6 @@ function currencyWidget() {
     fetch(api).then((response) => {
         return response.json();
     }).then((data) => { 
-        console.log(data);
-
         for(i=0; i < data.length; i++) {
             var ccy = document.createElement("div");
             ccy.innerText = data[i].ccy;
@@ -66,14 +63,64 @@ function currencyWidget() {
         }
     }).catch(error => {
         console.log(error);
-        });
-
-      update();
-}
-
-function update() {
-    setInterval(currencyWidget, 3600000);
+    });
 }
 
 currencyWidget();
+
+/* SearchWeather */
+
+window.onload = function() {
+    check();
+    showCityWeather(); 
+}
+    var cityWeatherList = [];
+    var searchBtn = document.getElementById("search-btn");
+    var weatherContainer = document.querySelector(".weather-container");
+     
+    function getWeatherData () {
+        var city = document.getElementById("search-input");
+        var api = `http://api.openweathermap.org/data/2.5/weather?q=${city.value}&APPID=a03e764ac19cc224e87a100ab518b340`;
+
+          fetch(api).then(response => {
+            return response.text();
+            }).then(data => {
+            var data = JSON.parse(data);
+            cityWeatherList.push(data);
+            localStorage.setItem("cities",JSON.stringify(cityWeatherList));
+            console.log(cityWeatherList);
+            showCityWeather();
+        })
+    }
+    
+    function check () {
+        if(localStorage.getItem("cities") != null) {
+            cityWeatherList = JSON.parse(localStorage.getItem("cities"));}  
+    }
+    function showCityWeather () {
+        var item = "";
+        for(let i=0; i< cityWeatherList.length; i++) {
+            item = item +'<div class = "city-weather">';
+            item = item + '<h2 class = "city-name">' + cityWeatherList[i].name + ', ' + cityWeatherList[i].sys.country + '</h2>';
+            item = item + '<div class = "city-weather-icon"> '+ '<img src="https://openweathermap.org/img/w/'+ cityWeatherList[i].weather[0].icon + '.png">' +'</div>';
+            item = item + '<div class = "city-weather-info">' + Math.round(cityWeatherList[i].main.temp - 273,15)+ " °C" +'</div>';
+            item = item + '<div class = "city-weather-info">' + cityWeatherList[i].weather[0].description+"<br>"+"humidity: "+cityWeatherList[i].main.humidity+"%"+'</div>'; 
+            item = item + '<div class = "city-weather-info">' + '<button dataArt ="' + [i] + '" class = "remove">Delete</button>'+'</div>';
+            item = item + '</div>';
+        }
+        weatherContainer.innerHTML = item;
+    }
+    
+    function remove (event) {
+    if (event.target.className === "remove") {
+    var attribut = event.target.getAttribute("dataArt");
+    cityWeatherList.splice([attribut],1);
+    showCityWeather();
+    localStorage.setItem("cities",JSON.stringify(cityWeatherList));
+        }
+    }
+    
+    document.addEventListener('click', remove);
+    searchBtn.addEventListener('click', getWeatherData);
+
 });
